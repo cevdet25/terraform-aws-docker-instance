91546bbf5573bee8b684afd4bb323aca0258c904
EC2 Default User <ec2-user@ip-172-31-94-62.ec2.internal>
2025-03-06 12:27:47 +0000

    new files added

---
+++ main.tf
@@ -0,0 +1,67 @@
+data "aws_ami" "amazon-linux-2023" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "tfmyec2" {
+  ami = data.aws_ami.amazon-linux-2023.id
+  instance_type = var.instance_type
+  count = var.num_of_instance
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  user_data = templatefile("${abspath.module}/userdata.sh", {myserver = var.server-name})
+  tags = {
+    Name = var.tag
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+  tags = {
+    Name = "${var.tag}-${count.index + 1}"
+  }
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    # The iterator argument (optional) sets the name of a temporary variable that represents the current element of the complex value. If omitted, the name of the variable defaults to the label of the dynamic block ("ingress" in the example above).
+
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port =0
+    protocol = "-1"
+    to_port =0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
